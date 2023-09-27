d1938d40b11c7303d9b3ce47a63e95c5bafd2fe8
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-27 08:46:28 +0300

    IoC DI setters value, references

--- src/main/java/by/flameksandr/MusicPlayer.java
+++ src/main/java/by/flameksandr/MusicPlayer.java
@@ -1,10 +1,34 @@
 package by.flameksandr;
 
 public class MusicPlayer {
+
+    private String name;
+    private int volume;
     private Music music;
 
-    public MusicPlayer(Music music) {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
+    }
+
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
     }
 
     public void playMusic() {
