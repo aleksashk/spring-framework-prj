2f4ac9dabd7d058d16ace5787def46e9563987ab
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-27 09:51:27 +0300

    beans lifecycle init-method destroy-method

--- src/main/java/by/flameksandr/ClassicalMusic.java
+++ src/main/java/by/flameksandr/ClassicalMusic.java
@@ -2,6 +2,14 @@ package by.flameksandr;
 
 public class ClassicalMusic implements Music{
 
+    public void initialization(){
+        System.out.println("Initialization");
+    }
+
+    public void destruction(){
+        System.out.println("Destruction");
+    }
+
     @Override
     public String getSong() {
         return "Playing Hungarian rhapsody";
--- src/main/java/by/flameksandr/Starter.java
+++ src/main/java/by/flameksandr/Starter.java
@@ -6,11 +6,15 @@ public class Starter {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+        ClassicalMusic music = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(music.getSong());
+        context.close();
 
-        System.out.println(musicPlayer1);
-        System.out.println(musicPlayer2);
-        System.out.println(musicPlayer1==musicPlayer2);
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        System.out.println(musicPlayer1);
+//        System.out.println(musicPlayer2);
+//        System.out.println(musicPlayer1==musicPlayer2);
     }
 }
