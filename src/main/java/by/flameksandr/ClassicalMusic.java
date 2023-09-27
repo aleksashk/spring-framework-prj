24ef0656abe1d27e6c7f703b34899d443b8d0904
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-27 10:56:56 +0300

    annotation @Scope(value = "prototype")

--- src/main/java/by/flameksandr/ClassicalMusic.java
+++ src/main/java/by/flameksandr/ClassicalMusic.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("classicalMusic")
-@Scope(value = "singleton")
+@Scope(value = "prototype")
 public class ClassicalMusic implements Music {
 
     private ClassicalMusic() {
